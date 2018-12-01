298d13d9d4ad98a79dfa47443a7c061983555e16
Hem1sphere <fitz0126@hotmail.com>
2018-12-01 16:42:06 +0000

    I'm dumb
    First mock on OperationsTeam working

--- trafficmon/tests/com/trafficmon/PowerLegacyTest.java
+++ trafficmon/tests/com/trafficmon/PowerLegacyTest.java
@@ -3,6 +3,8 @@ package com.trafficmon;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
@@ -11,8 +13,7 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import static org.junit.Assert.assertFalse;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest(OperationsTeam.class)
-
+@PrepareForTest({CongestionChargeSystem.class, OperationsTeam.class})
 public class PowerLegacyTest{
 
         @Test
@@ -26,6 +27,7 @@ public class PowerLegacyTest{
                 Vehicle testVehicle = Vehicle.withRegistration("STARTOVER");
                 congestionChargeSystem.vehicleEnteringZone(testVehicle);
                 congestionChargeSystem.vehicleEnteringZone(testVehicle);
+                congestionChargeSystem.calculateCharges();
 //                assertFalse(congestionChargeSystem.pubCheckOrdering());
                 Mockito.verify(mockedOpsTeam, Mockito.times(1)).triggerInvestigationInto(testVehicle);
         }
